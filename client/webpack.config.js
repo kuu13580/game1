48fca100fc8061eeb39b16688baa6d4a3bb2fd59
kuu13580 <13580kuu@gmail.com>
2023-07-21 15:41:45 +0900

    delete typescript

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,15 +1,6 @@
 const path = require('path');
 module.exports = {
   entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
   resolve: {
     extensions: [ '.ts', '.tsx', '.js' ]
   },
