fecce821efec58f52f8b524860304794002caa79
kuu13580 <13580kuu@gmail.com>
2023-08-13 01:32:26 +0900

    delete unneccesary lines

--- client/src/scenes/Title.js
+++ client/src/scenes/Title.js
@@ -1,25 +1,5 @@
 const D_WIDTH = 800;
 const D_HEIGHT = 600;
-
-// // 1, Phaser3の設定データ
-// const config = {
-//   type: Phaser.AUTO,
-//   width: 800,
-//   height: 600,
-//   physics: {
-//       default: 'arcade',
-//       arcade: {
-//           gravity: { y: 200 }
-//       }
-//   },
-//   scene: {
-// 		preload: preload,// 素材の読み込み時の関数
-// 		create: create,// 画面が作られた時の関数
-// 	},
-// };
-
-// // 2, Phaser3オブジェクトを作る
-// let phaser = new Phaser.Game(config);
 import "phaser";
 
 export class Title extends Phaser.Scene {
@@ -61,24 +41,3 @@ export class Title extends Phaser.Scene {
 //        }, this);
 //      }
 //   };
-  
-//   //Scene B
-//  export class SceneB extends Phaser.Scene {
-  
-//     constructor (){
-//             super({ key: 'SceneB' });
-//      }
-  
-//     create(){
-      
-//       let sceneName = this.add.text(150, 70, 'SceneB').setFontSize(30).setFontFamily("Arial").setOrigin(0.5).setInteractive();
-      
-//       let change = this.add.text(150, 130, 'Change Scene!').setFontSize(20).setFontFamily("Arial").setOrigin(0.5).setInteractive();
-      
-//       change.on('pointerdown', function (pointer) {
-//       this.scene.start('SceneA');
-//        }, this);
-//      }
-     
-//   };
-  
