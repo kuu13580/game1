c7b64e28c714f92cf3013a57f5a272f10abc7b88
Ho-Ko-Ri3 <katatsumuru117@gmail.com>
2023-08-16 17:15:51 +0900

    六角形の描画できて草

--- client/src/scenes/Example.js
+++ client/src/scenes/Example.js
@@ -1,5 +1,7 @@
 import "phaser";
 
+const SQRT3 = Math.sqrt(3);
+
 export class Example extends Phaser.Scene
 {
     constructor ()
@@ -35,3 +37,57 @@ export class Example extends Phaser.Scene
       particles.startFollow(logo);
     }
 }
+
+// バトルフィールド共通
+export class Field extends Phaser.Scene {
+    constructor (){
+        super({ key: 'Field' }
+        );
+    }
+
+    create(){
+        const r = 50;
+        const xMargin = 10;
+        const yMargin = 7
+        const height = yMargin + 3 * r / 2;
+
+        this.drawHexagonLine(200, 200, r, xMargin, 5);
+        this.drawHexagonLine(200 + xMargin / 2 + SQRT3 * r / 2, 200 + height, r, xMargin, 5);
+        this.drawHexagonLine(200, 200 + height * 2, r, xMargin, 5);
+    }
+
+    // 六角形を描画する
+    drawHexagon(x, y, r) {
+        const width = SQRT3 * r;
+        const data = [[width/2,0], [width,r/2], [width,3*r/2], [width/2,2*r], [0,3*r/2], [0,r/2]];
+        const hex = this.add.polygon(x, y, data, "0xffffff").setOrigin(0.5);
+        return hex;
+    }
+
+    // n個六角形を描画する
+    drawHexagonLine(x, y, r, margin, n) {
+        for(let i = 0; i < n; i++){
+            const X = x + margin * i + SQRT3 * r * i;
+            this.drawHexagon(X, y, r);
+        }
+    }
+};
+
+
+
+
+export class PlayerSelect extends Phaser.Scene {
+constructor (){
+    super({ key: 'PlayerSelect' });
+}
+
+
+create(){
+    let text = this.add.text(30, 30, "Player1")    
+    let change = this.add.text(400, 350, 'Change Scene!').setFontSize(50).setFontFamily("Arial").setOrigin(0.5).setInteractive();
+    
+    change.on('pointerdown', function (pointer) {
+    this.scene.start('Field');
+        }, this)
+    }
+};
--- client/src/scenes/Title.js
+++ client/src/scenes/Title.js
@@ -1,26 +1,9 @@
 const D_WIDTH = 800;
 const D_HEIGHT = 600;
 
-// // 1, Phaser3の設定データ
-// const config = {
-//   type: Phaser.AUTO,
-//   width: 800,
-//   height: 600,
-//   physics: {
-//       default: 'arcade',
-//       arcade: {
-//           gravity: { y: 200 }
-//       }
-//   },
-//   scene: {
-// 		preload: preload,// 素材の読み込み時の関数
-// 		create: create,// 画面が作られた時の関数
-// 	},
-// };
-
-// // 2, Phaser3オブジェクトを作る
-// let phaser = new Phaser.Game(config);
 import "phaser";
+import { Input } from "phaser";
+
 
 export class Title extends Phaser.Scene {
   constructor (){
@@ -35,50 +18,13 @@ export class Title extends Phaser.Scene {
     console.log("create!!");
     const bgImage = this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');
     bgImage.setDisplaySize(D_WIDTH, D_HEIGHT);
+
+    let sceneName = this.add.text(400, 280, 'Player').setFontSize(80).setFontFamily("Arial").setOrigin(0.5).setInteractive();
+      
+    let change = this.add.text(400, 350, 'Change Scene!').setFontSize(50).setFontFamily("Arial").setOrigin(0.5).setInteractive();
+      
+    change.on('pointerdown', function (pointer) {
+    this.scene.start('Example');
+    }, this);
   }
 }
-
-
-
-
-
-// //Scene A
-// export class SceneA extends Phaser.Scene {
-
-//     constructor (){
-//             super({ key: 'SceneA' });
-//      }
-
-  
-//     create(){
-      
-//       let sceneName = this.add.text(400, 280, 'SceneA').setFontSize(80).setFontFamily("Arial").setOrigin(0.5).setInteractive();
-      
-//       let change = this.add.text(400, 350, 'Change Scene!').setFontSize(50).setFontFamily("Arial").setOrigin(0.5).setInteractive();
-      
-//       change.on('pointerdown', function (pointer) {
-//       this.scene.start('Example');
-//        }, this);
-//      }
-//   };
-  
-//   //Scene B
-//  export class SceneB extends Phaser.Scene {
-  
-//     constructor (){
-//             super({ key: 'SceneB' });
-//      }
-  
-//     create(){
-      
-//       let sceneName = this.add.text(150, 70, 'SceneB').setFontSize(30).setFontFamily("Arial").setOrigin(0.5).setInteractive();
-      
-//       let change = this.add.text(150, 130, 'Change Scene!').setFontSize(20).setFontFamily("Arial").setOrigin(0.5).setInteractive();
-      
-//       change.on('pointerdown', function (pointer) {
-//       this.scene.start('SceneA');
-//        }, this);
-//      }
-     
-//   };
-  
