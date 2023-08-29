618bf6641731b60dfdf0aa971dfc97f0b27f6344
kuu13580 <13580kuu@gmail.com>
2023-08-29 22:51:44 +0900

    add ルーム作成・入室を仮実装

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 
 class CServer {
   constructor() {
+    this.rooms = [];
     this.app = express();
     this.server = http.createServer(this.app);
     this.io = new Server(this.server, {
@@ -19,6 +20,14 @@ class CServer {
     this.io.on("connection", (socket) => {
       // send a message to the client
       socket.emit("first", "connect");
+      // 部屋作成
+      socket.on("create", (userName) => {
+        this.create(socket, userName);
+      });
+      // 部屋に入室する
+      socket.on("enter", (userName, roomId) => {
+        this.enter(socket, userName, roomId);
+      });
     });
   }
 
@@ -27,6 +36,51 @@ class CServer {
       console.log(`listening on port ${PORT}`);
     });
   }
+
+  create(socket, userName){
+    if (userName == "") {
+      this.io.to(socket.id).emit("notifyError", "名前を入力してください");
+      return;
+    }
+    const roomId = generateRoomId();
+    const user = { id: socket.id, name: userName, room: roomId };
+    const room = {
+      id: roomId,
+      users: [user],
+      turnUserIndex: 0,
+      posts: [],
+    };
+    this.rooms.push(room);
+    socket.join(roomId);
+    this.io.to(socket.id).emit("updateRoom", room);
+    console.log("room", this.rooms, "created");
+  }
+
+  enter(socket, userName, roomId) {
+    if (userName == "") {
+      this.io.to(socket.id).emit("notifyError", "名前を入力してください");
+      return;
+    }
+    const roomIndex = this.rooms.findIndex((r) => r.id == roomId);
+    if (roomIndex == -1) {
+      this.io.to(socket.id).emit("notifyError", "部屋が見つかりません");
+      return;
+    }
+    const user = { id: socket.id, name: userName, room: roomId };
+    this.rooms[roomIndex].users.push(user);
+    socket.join(rooms[roomIndex].id);
+    this.io.to(socket.id).emit("updateRoom", rooms[roomIndex]);
+    console.log("room", this.rooms[roomIndex], "entered");
+  }
+}
+
+function generateRoomId() {
+  const id = Math.floor(Math.random() * 8999 + 1000);
+  if (rooms.some((r) => r.id == id)) {
+    // ランダムに生成したidが既に存在する場合は作り直す
+    return generateRoomId();
+  }
+  return id;
 }
 
 const server = new CServer();
